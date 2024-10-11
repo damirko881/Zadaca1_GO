1ffdd46f2b1198a9ac7e9926ec82b5cdd189fd84
Damir Mihajlović <74556243+damirko881@users.noreply.github.com>
2024-10-11 19:07:42 +0200

    Initial commit

---
+++ Zadatak2/main.go
@@ -0,0 +1,21 @@
+/*2. Declare two variables, `firstName` and `lastName` assign them with wanted values.
+Declare constant named `fullname`
+Combine constant and both strings into a full name by concatenating strings with a space in between and print them out.*/
+
+package main
+
+import "fmt"
+
+func main() {
+	var firstName string
+	firstName = "Damir"
+
+	var lastName string
+	lastName = "Mihajlović"
+
+	var fullname string
+	fullname = firstName + " " + lastName
+
+	fmt.Println(fullname)
+
+}
